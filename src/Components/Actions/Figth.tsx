291519bb7928e23845045dbd72e686d5c17fddfc
jorge antonio <jorgeantonio.acll@gmail.com>
2022-08-27 19:31:46 -0300

    vendo animações

--- src/Components/Actions/Figth.tsx
+++ src/Components/Actions/Figth.tsx
@@ -1,11 +1,14 @@
 import "../../styles/components/figth.sass"
-import lanca from "../../imgs/lança.jpg"
+import lanca from "../../imgs/lança-removebg.png"
 
 function Figth() {
   return (
-    <div className="interaction__card__item">
-      <img src={lanca}alt="" id="lanca" />
-    <p>* (Acho que Você Realmente não quer isso)</p>
+    <div className="interaction__card__item " id="fight__card">
+      <img src={lanca} alt="" id="lanca" />
+      <img src={lanca} alt="" id="lanca2" />
+     <div className="fight__div"><p>* (Acho que Você Realmente não quer isso)</p></div>
+      <img src={lanca} alt="" id="lanca__direita__2" />
+      <img src={lanca} alt="" id="lança__direita" />
     </div>
   )
 }
