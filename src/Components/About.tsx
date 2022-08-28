484887c017e4b6bcbc415ddfcd3ea0ad033c0e39
jorge antonio <jorgeantonio.acll@gmail.com>
2022-08-28 16:26:04 -0300

    ajustes about

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,36 +1,43 @@
 
+import { useState } from "react"
 import "../styles/components/about.sass"
 import hearth from "../imgs/pulsehearth.gif"
 import andar from "../imgs/toriel_frisk_andando.gif"
 import asriel from "../imgs/asriel.png"
-function About() {
 
-  const aboutMessage = document.getElementById("about__about") 
-  const message = document.getElementById("about__message")
-  const saveHour = document.getElementById("hSave")
+
+
+function About() {
   const d = new Date()
   let hour = d.getHours()
   let minutes = d.getMinutes()
 
-  const save = () => {
-    if (aboutMessage && message  && saveHour) {
-      console.log("carregado")
-      aboutMessage.style.display= "none"
-      message.style.display="flex"
-      saveHour.textContent = `${hour}:${minutes}`
-    }else{
-      console.log("carregando")
-    }
-  }
-  
 
-  const retur = () => {
-    if (aboutMessage && message){
-      console.log("carregado")
-      aboutMessage.style.display= "flex"
-      message.style.display="none"
-    }else{
-      console.log("carregando")
+  const [key, setKey] = useState<number>(0)
+
+  const RenderMessage = () => {
+    if(key == 0) {
+      return (
+
+        <div id="about__about">
+          <p>* (interactive buttons)</p>
+          <p>* (it fills you with determination) </p>
+        </div>
+
+      )
+    }else if(key == 1) {
+      return (
+
+        <div id="about__message">
+          <p>* (hello my name is jorge, i'm 19 years old) </p>
+          <p>* (I'm studying software engineering and I'm learning technologies as html, css, javascript, react, node, postgres, sass)</p>
+          <p>* (besides starting to learn java)</p>
+          <a href="#interaction">Click for continue</a>
+        </div>
+
+      )
+    } else {
+      return <div><p>Problems</p></div>
     }
   }
   
@@ -47,27 +54,23 @@ function About() {
         <p className='about__card__item__p'>Local-Home</p>
 
         <div className='about__card__item'>
-          <button onClick={save}>
+          <button onClick={()=>{
+            setKey(1)
+            console.log(key)
+          }}>
             <span><img src={hearth} alt="" />Save</span>
           </button>
-          <button onClick={retur}>
+          <button onClick={()=>{
+            setKey(0)
+            console.log(key)
+          }}>
             <span><img src={hearth} alt="" />Return</span>
           </button>
         </div>
       </article>
       <img className="img" src={asriel} alt="" />
 
-      <div id="about__about">
-        <p>* (interactive buttons)</p>
-        <p>* (it fills you with determination) </p>
-      </div>
-
-      <div id='about__message'>
-        <p>* hello my name is jorge, i'm 19 years old </p>
-        <p>* I'm studying software engineering and I'm learning technologies as html, css, javascript, react, node, postgres, sass</p>
-        <p>* besides starting to learn java</p>
-        <a href="#interaction">Click for continue</a>
-      </div>
+      <RenderMessage />
 
     </section>
   )
