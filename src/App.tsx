c870827855c3b88f09d2a1a2bc56c737bd9258ec
jorge antonio <jorgeantonio.acll@gmail.com>
2022-09-01 10:57:06 -0300

    Recriando

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import About from "./Components/About"
-import Start from "./Components/Start"
+
 function App() {
   
 
   return (
     <div id="portfolio">
-    <Start/>
-    <About/>
+    
     </div>
   )
 }
